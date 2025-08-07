7ffacf0ca05deb32637f654b240d1d9e4c176fc4
IDGS-904-22000819 <78749@alumnos.utleon.edu.mx>
2025-08-07 15:17:50 -0600

    Correcciones finales

--- Secure_App_Backend/obj/Debug/net9.0/Secure_App_Backend.AssemblyInfo.cs
+++ Secure_App_Backend/obj/Debug/net9.0/Secure_App_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Secure_App_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8d5af3951c22ba7a31fad3975a0ceff3bb7eec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Secure_App_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Secure_App_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
